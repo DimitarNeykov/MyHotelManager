e3e9b7fc7ac3d8b8cfdde248eee98588ba0e2f4c
dimitarneikov <dimitarneikov@gmail.com>
2020-11-25 12:02:44 +0200

    Seed new Manager role and Receptionist role

--- src/MyHotelManager.Common/GlobalConstants.cs
+++ src/MyHotelManager.Common/GlobalConstants.cs
@@ -5,5 +5,9 @@
         public const string SystemName = "MyHotelManager";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string ManagerRoleName = "Manager";
+
+        public const string ReceptionistRoleName = "Receptionist";
     }
 }
--- src/Web/MyHotelManager.Web.ViewModels/Cleaning/CleaningRoomViewModel.cs
+++ src/Web/MyHotelManager.Web.ViewModels/Cleaning/CleaningRoomViewModel.cs
@@ -13,8 +13,10 @@
 
         public RoomType RoomType { get; set; }
 
+        public int HotelCleaningPerDays { get; set; }
+
         public Reservation Reservation => this.Reservations.FirstOrDefault(x =>
-            (DateTime.Now.Date - x.ArrivalDate.Date).Days % 3 == 0 && x.ArrivalDate.Date < DateTime.Now.Date ||
+            (DateTime.Now.Date - x.ArrivalDate.Date).Days % this.HotelCleaningPerDays == 0 && x.ArrivalDate.Date < DateTime.Now.Date ||
             x.ReturnDate.Date == DateTime.Now.Date);
 
         public IEnumerable<Reservation> Reservations { get; set; }
