e111a7591fffd437f83cf6dc878765ff4a052cf6
dimitarneikov <dimitarneikov@gmail.com>
2020-12-18 14:36:50 +0200

    Working on services tests

---
+++ src/Tests/MyHotelManager.Services.Data.Tests/RoomTypeServiceTest.cs
@@ -0,0 +1,39 @@
+﻿namespace MyHotelManager.Services.Data.Tests
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+    using MyHotelManager.Data;
+    using MyHotelManager.Data.Models;
+    using MyHotelManager.Data.Repositories;
+    using MyHotelManager.Services.Mapping;
+    using Xunit;
+
+    public class RoomTypeServiceTest
+    {
+        [Fact]
+        public async Task TestGetAllRoomTypes()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("RoomTypes");
+
+            var roomTypeRepository = new EfDeletableEntityRepository<RoomType>(new ApplicationDbContext(options.Options));
+
+            await roomTypeRepository.AddAsync(new RoomType { Name = "Double" });
+            await roomTypeRepository.AddAsync(new RoomType { Name = "Single" });
+            await roomTypeRepository.SaveChangesAsync();
+
+            var roomTypeService = new RoomTypesService(roomTypeRepository);
+            AutoMapperConfig.RegisterMappings(typeof(MyTestRoomType).Assembly);
+            var roomTypes = roomTypeService.GetAll<MyTestRoomType>();
+
+            Assert.Equal(2, roomTypes.Count());
+        }
+
+        public class MyTestRoomType : IMapFrom<RoomType>
+        {
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ src/Tests/MyHotelManager.Services.Data.Tests/StarsServiceTest.cs
@@ -0,0 +1,39 @@
+﻿namespace MyHotelManager.Services.Data.Tests
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+    using MyHotelManager.Data;
+    using MyHotelManager.Data.Models;
+    using MyHotelManager.Data.Repositories;
+    using MyHotelManager.Services.Mapping;
+    using Xunit;
+
+    public class StarsServiceTest
+    {
+        [Fact]
+        public async Task TestGetAllStars()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("Stars");
+
+            var starRepository = new EfDeletableEntityRepository<Stars>(new ApplicationDbContext(options.Options));
+
+            await starRepository.AddAsync(new Stars { Name = "One" });
+            await starRepository.AddAsync(new Stars { Name = "Two" });
+            await starRepository.SaveChangesAsync();
+
+            var starsService = new StarsService(starRepository);
+            AutoMapperConfig.RegisterMappings(typeof(MyTestStars).Assembly);
+            var stars = starsService.GetAll<MyTestStars>();
+
+            Assert.Equal(2, stars.Count());
+        }
+
+        public class MyTestStars : IMapFrom<Stars>
+        {
+            public string Name { get; set; }
+        }
+    }
+}
