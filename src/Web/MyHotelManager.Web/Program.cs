360f2a3b9ad8867b4aa8734772e0033011526364
Dimitar Neykov <dimitarneikov@gmail.com>
2021-04-17 12:28:31 +0300

    Change Program.cs to original

--- src/Web/MyHotelManager.Web/Program.cs
+++ src/Web/MyHotelManager.Web/Program.cs
@@ -13,22 +13,6 @@
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                    {
-                        config.Sources.Clear();
-
-                        var env = hostingContext.HostingEnvironment;
-
-                        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                            .AddJsonFile(
-                                $"appsettings.{env.EnvironmentName}.json",
-                                optional: true, reloadOnChange: true);
-
-                        if (args != null)
-                        {
-                            config.AddCommandLine(args);
-                        }
-                    })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
